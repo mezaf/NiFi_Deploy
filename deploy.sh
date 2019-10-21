5a28fed7c67b7528075594e92c624c60ecbeee1a
Fernando Meza <fmeza@HEXACTA.COM>
2019-10-21 17:22:06 -0300

    fix env variables

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,8 @@ tar xvzf nifi-registry-0.3.0-bin.tar.gz
 rm -f nifi-1.9.0-bin.tar.gz
 rm -f nifi-registry-0.3.0-bin.tar.gz
 cp /home/mezaf/Documents/NiFi_script_Deploy/flow.xml.gz /opt/nifi-1.9.0/conf
+apt-get install openjdk-8-jdk
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+export JRE_HOME=/user/lib/jvm/java-8-openjdk-amd64/jre/
 /opt/nifi-1.9.0/bin/nifi.sh start
 /opt/nifi-registry-0.3.0/bin/nifi-registry.sh start
