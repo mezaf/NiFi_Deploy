c396e58ea191401058ef1cb90e6f12671f841048
Fernando Meza <fmeza@HEXACTA.COM>
2019-10-22 16:37:50 -0300

    change download handle

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,9 @@
-cd /opt
-wget https://archive.apache.org/dist/nifi/1.9.0/nifi-1.9.0-bin.tar.gz
-wget https://archive.apache.org/dist/nifi/nifi-registry/nifi-registry-0.3.0/nifi-registry-0.3.0-bin.tar.gz
-tar xvzf nifi-1.9.0-bin.tar.gz
-tar xvzf nifi-registry-0.3.0-bin.tar.gz
-rm -f nifi-1.9.0-bin.tar.gz
-rm -f nifi-registry-0.3.0-bin.tar.gz
+wget https://archive.apache.org/dist/nifi/1.9.0/nifi-1.9.0-bin.tar.gz -P /opt
+wget https://archive.apache.org/dist/nifi/nifi-registry/nifi-registry-0.3.0/nifi-registry-0.3.0-bin.tar.gz -P /opt
+tar -C /opt -xvzf /opt/nifi-1.9.0-bin.tar.gz
+tar -C /opt -xvzf /opt/nifi-registry-0.3.0-bin.tar.gz
+rm -f /opt/nifi-1.9.0-bin.tar.gz
+rm -f /opt/nifi-registry-0.3.0-bin.tar.gz
 cp flow.xml.gz /opt/nifi-1.9.0/conf
 cp archive/* /opt/nifi-1.9.0/archive
 cp nifi-registry.mv.db /opt/nifi-registry-0.3.0/database/nifi-registry.mv.db
